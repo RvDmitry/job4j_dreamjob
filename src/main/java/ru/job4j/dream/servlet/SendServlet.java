8f9e9ef93b541bbd6cb5508175908925129f96fa
RvDmitry <rvdmitry@gmail.com>
2021-02-01 21:22:53 +0600

    Переименовал сервлет. Также добавил возможность работы с символами unicode.

--- src/main/java/ru/job4j/dream/servlet/SendServlet.java
+++ src/main/java/ru/job4j/dream/servlet/SendServlet.java
@@ -12,18 +12,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
- * Class UserSendServlet
+ * Class SendServlet
  * Сервлет принимает и отправляет данные в JSON формате.
  * @author Dmitry Razumov
  * @version 1
  */
-public class UserSendServlet extends HttpServlet {
+public class SendServlet extends HttpServlet {
     /**
      * Поле создает логер.
      */
-    private static final Logger LOG = LoggerFactory.getLogger(UserSendServlet.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(SendServlet.class.getName());
 
     /**
      * Метод принимает и отправляет данные в формате JSON.
@@ -36,8 +37,11 @@ public class UserSendServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
         String json = "";
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()))) {
+        try (BufferedReader br = new BufferedReader(
+                new InputStreamReader(req.getInputStream(), StandardCharsets.UTF_8)
+        )) {
             json = br.readLine();
             LOG.info("Получены данные: {}", json);
         } catch (IOException ex) {
